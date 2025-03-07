f0ba864a592d6ad0c27e4198949ebd432040b877
Shiv Govil <shivgov@umich.edu>
2025-03-06 22:42:07 -0500

    progress on flat map

--- flat_map/flat_map.hpp
+++ flat_map/flat_map.hpp
@@ -1,13 +1,57 @@
 #ifndef swiss_table_HPP
 #define swiss_table_HPP
 
-class swiss_table {
+#include <cstdint>
+#include <cfloat>
+#include <xxhash.h>
+
+template <typename K, typename V>
+class flat_map {
   public:
-    swiss_table();
-    ~swiss_table();
+    flat_map(int _capacity = INITIAL_CAP) : capacity(_capacity) {
+      entries = new entry[capacity];
+      cache = new uint8_t[capacity];
+    }
+    ~flat_map();
+
+    void insert(const K &key, const V &value) {}
+
+    void find(const K &key) {}
+
+    void clear() {}
+
+    V& operator[](const K &key) {}
+
+    const V& operator[](const K &key) const {}
 
   private:
-    // Add private members here
+    static constexpr float LOAD_FACTOR = 0.875;
+    static constexpr float GROWTH_FACTOR = 1.5;
+    static constexpr uint8_t INITIAL_CAP = 128;
+
+    struct entry {
+      K key;
+      V value;
+      // status?
+    };
+
+    class Iterator {
+
+    };
+
+    entry *entries;
+    uint8_t *cache;
+    int capacity;
+    int size{};
+
+    void grow() {
+
+    }
+
+    uint64_t hash() {
+
+    }
+
 };
 
 #endif // swiss_table_HPP
