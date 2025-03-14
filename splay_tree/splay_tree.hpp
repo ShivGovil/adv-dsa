3d64a4b19d619d6cf62113366c308f8236ac8af2
Shiv Govil <shivgov@umich.edu>
2025-03-14 16:55:46 -0400

    the minimal amount of code to represent a start on splay tree looooool

--- splay_tree/splay_tree.hpp
+++ splay_tree/splay_tree.hpp
@@ -1,13 +1,43 @@
 #ifndef splay_tree_HPP
 #define splay_tree_HPP
 
+#include <memory>
+template <typename K, typename V>
 class splay_tree {
   public:
-    splay_tree();
-    ~splay_tree();
+    splay_tree() : root(nullptr) {}
+
+    void push(const K &key, const V &value) {
+      if (!root) {
+        root = make_shared<node>(node{key, value, nullptr, nullptr, nullptr});
+        return;
+      }
+
+    }
+
+    void remove(const K &key) {
+
+    }
+
+    V& find(const K &key) {
+
+    }
+
+    const V& find(const K &key) {
+
+    }
 
   private:
-    // Add private members here
+    struct node {
+      K key;
+      V value;
+      std::shared_ptr<node> left, right;
+      // If the parent ptr was shared, then we would have circular references
+      // This would hinder the destruction process
+      std::weak_ptr<node> parent;
+    };
+
+    std::shared_ptr<node> root;
 };
 
-#endif // splay_tree_HPP
+#endif
