d0861f509a8f008d7cd297686804ca25ceba8b35
Shiv Govil <shivgov@umich.edu>
2025-05-10 18:50:05 -0400

    sqrt decomp

--- sqrt_decomp/main.cpp
+++ sqrt_decomp/main.cpp
@@ -1,7 +1,14 @@
 #include "sqrt_decomp.hpp"
 #include <iostream>
+#include <vector>
 
 int main() {
-    // Add your code here
-    return 0;
+  std::vector<double> nums = {0.5, 1, 7, 15, 0.2, 1, 1.2, 4.3, 6, 10, 14};
+  sqrt_decomp<double> s(nums);
+
+  s.print();
+  std::cout << s.query(0, 7) << '\n';
+  s.update_elt(5, 7.5);
+  std::cout << s.query(0, 7) << '\n';
+  return 0;
 }
--- sqrt_decomp/sqrt_decomp.hpp
+++ sqrt_decomp/sqrt_decomp.hpp
@@ -1,13 +1,61 @@
-#ifndef sqrt_decomp_HPP
-#define sqrt_decomp_HPP
+#pragma once
 
+#include <cmath>
+#include <cassert>
+#include <vector>
+#include <iostream>
+
+template<typename T>
 class sqrt_decomp {
-    public:
-        sqrt_decomp();
-        ~sqrt_decomp();
-    
-    private:
-        // Add private members here
-};
+  std::vector<T> nums;
+  std::vector<T> data;
+  size_t N;
+  size_t S;
 
-#endif // sqrt_decomp_HPP
+public:
+  sqrt_decomp(const std::vector<T> &nums_) :
+    nums(nums_),
+    N(nums.size()),
+    S(static_cast<size_t>(std::ceil(std::sqrt(N)))) {
+
+    data.resize((N + S - 1) / S);
+    for (size_t i = 0; i < N; ++i) {
+      data[i / S] += nums[i];
+    }
+  }
+
+  T query(size_t l, size_t r) {
+    T sum{};
+    size_t blk_l = l / S;
+    size_t blk_r = r / S;
+    if (blk_l == blk_r) {
+      for (size_t i = l; i < r; ++i)
+        sum += nums[i];
+    } else {
+      size_t end = std::min((blk_l + 1) * S, r + 1);
+      for (size_t i = l; i <= end; ++i)
+        sum += nums[i];
+      for (size_t i = blk_l + 1; i <= blk_r - 1; ++i)
+        sum += data[i];
+      for (size_t i = blk_r * S; i <= r && i < nums.size(); ++i)
+        sum += nums[i];
+    }
+
+    return sum;
+  }
+
+  void update_elt(size_t index, T new_value) {
+    assert(index < N);
+    data[index / S] += new_value - nums[index];
+    nums[index] = new_value;
+  }
+
+  void print() {
+    for (size_t i = 0; i < data.size(); ++i) {
+      std::cout << data[i] << ' ';
+    }
+    std::cout << '\n';
+  }
+
+  // void update_interval(size_t l, size_t r) {}
+};
