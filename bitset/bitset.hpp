336a1b31c46e997335096529713708bbaf7b3b9b
Shiv Govil <shivgov@umich.edu>
2025-05-09 22:32:48 -0400

    random progress on bitset

--- bitset/bitset.hpp
+++ bitset/bitset.hpp
@@ -1,13 +1,52 @@
-#ifndef bitset_HPP
-#define bitset_HPP
+#pragma once
 
+#include <climits>
+#include <cstddef>
+#include <cstdint>
+
+namespace shiv {
+
+using chunk = uint64_t;
+static constexpr uint8_t BITS_PER_CHUNK = sizeof(chunk) * CHAR_BIT;
+
+template <size_t B>
+requires(B > 0)
 class bitset {
-  public:
-    bitset();
-    ~bitset();
+  size_t chunks, bits;
+  chunk *data;
 
-  private:
-    // Add private members here
+  size_t chunk_index(size_t bit);
+  size_t bit_index(size_t bit);
+
+  class bit_ref {
+
+  };
+
+public:
+  bitset() :
+    chunks((B + BITS_PER_CHUNK - 1) / BITS_PER_CHUNK),
+    bits(B),
+    data(new chunk[chunks]{}) {}
+
+  ~bitset() {
+    delete[] data;
+  }
+
+  bool test(size_t bit) {
+    if (bit > bits) return false;
+    
+
+  }
+  void flip(size_t bit);
+  void set(size_t bit, bool value = true) {
+    if (bit > bits) return;
+    if (value) {
+
+    }
+  }
+  size_t count();
+  void reset();
+  // all, any, none
 };
 
-#endif // bitset_HPP
+}
