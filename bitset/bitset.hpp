aade7fe44cf77ff45e04745abd37ed32f8a9be84
Shiv Govil <shivgov@umich.edu>
2025-05-10 17:53:48 -0400

    bitset basically done

--- bitset/bitset.hpp
+++ bitset/bitset.hpp
@@ -1,69 +1,260 @@
 #pragma once
 
+#include <cassert>
 #include <climits>
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
+#include <iostream>
 
 namespace shiv {
+// TODO: Binary operators + equality
 
-using chunk = uint64_t;
+using chunk = uint32_t; // To use __builtin_popcount()
+constexpr chunk CHUNK_MAX = static_cast<chunk>(-1);
 static constexpr uint8_t BITS_PER_CHUNK = sizeof(chunk) * CHAR_BIT;
 
-template <size_t B>
-requires(B > 0)
 class bitset {
   size_t n_chunks, n_bits;
-  chunk *data;
+  chunk *data{};
 
-  size_t chunk_index(size_t bit) {
+  [[nodiscard]] size_t chunk_index(size_t bit) const {
     return bit / BITS_PER_CHUNK;
   }
-  size_t bit_index(size_t bit) {
+  [[nodiscard]] size_t bit_index(size_t bit) const {
     return bit % BITS_PER_CHUNK;
   }
 
   class bit_ref {
+    chunk &e, mask; // NOLINT
 
+  public:
+    bit_ref(chunk &e_, chunk mask_) : e(e_), mask(mask_) {}
+
+    operator bool() const { return e & mask; }
+
+    bit_ref &operator=(bool b) {
+      b ? e |= mask : e &= ~mask;
+      return *this;
+    }
+
+    bool operator!() const { return !(e & mask); }
+
+    bool operator==(bool rhs) const {
+      return static_cast<bool>(*this) == rhs;
+    }
+
+    bool operator!=(bool rhs) const {
+      return !(*this == rhs);
+    }
+
+    bool operator==(int rhs) const {
+      return *this == static_cast<bool>(rhs);
+    }
+
+    bool operator!=(int rhs) const {
+      return !(*this == rhs);
+    }
   };
 
 public:
-  bitset() :
+  bitset(size_t B) :
     n_chunks((B + BITS_PER_CHUNK - 1) / BITS_PER_CHUNK),
     n_bits(B),
     data(new chunk[n_chunks]{}) {}
+
+  bitset(const bitset &other) :
+    n_chunks(other.n_chunks),
+    n_bits(other.n_bits),
+    data(new chunk[n_chunks]) {
+    memcpy(data, other.data, n_chunks * sizeof(chunk));
+  }
+
+  bitset &operator=(const bitset &other) {
+    if (&other == this) return *this;
+
+    n_chunks = other.n_chunks;
+    n_bits = other.n_bits;
+    delete[] data;
+    data = new chunk[n_chunks];
+    memcpy(data, other.data, n_chunks * sizeof(chunk));
+
+    return *this;
+  }
+
+  bitset &operator=(bitset &&other) noexcept {
+    if (&other == this) return *this;
+
+    n_chunks = other.n_chunks;
+    n_bits = other.n_bits;
+    delete[] data;
+    data = other.data;
+    other.data = nullptr;
+    other.n_chunks = 0;
+    other.n_bits = 0;
+
+    return *this;
+  }
 
   ~bitset() {
     delete[] data;
   }
 
-  bool test(size_t bit) {
-    if (bit > n_bits) return false;
-
-    
-    
-
+  bit_ref operator[](const size_t &bit) {
+    return {data[chunk_index(bit)], static_cast<chunk>(1) << bit_index(bit)};
   }
 
-  void flip(size_t bit) {}
+  bool operator[](const size_t &bit) const {
+    return test(bit);
+  }
 
-  void set(size_t bit, bool value = true) {
-    if (bit > n_bits) return;
-    if (value) {
-      data[chunk_index(bit)] |= (1LL << (BITS_PER_CHUNK - bit_index(bit)));
-    } else {
-      data[chunk_index(bit)] &= ~(1LL << (BITS_PER_CHUNK - bit_index(bit)));
+  bitset &operator<<=(size_t shift) {
+    if (!shift || none()) return *this;
+
+    if (shift >= size()) {
+      reset();
+      return *this;
+    }
+
+    size_t chunk_shift = shift / BITS_PER_CHUNK;
+    size_t bit_shift = shift % BITS_PER_CHUNK;
+
+    if (chunk_shift > 0) {
+      for (size_t c = n_chunks; c-- > chunk_shift;) {
+        data[c] = data[c - chunk_shift];
+      }
+      memset(data, 0, chunk_shift * sizeof(chunk));
+    }
+
+    if (bit_shift > 0) {
+      chunk carry{};
+      for (size_t c = 0; c < n_chunks; ++c) {
+        chunk next_carry = (data[c] >> (BITS_PER_CHUNK - bit_shift)) & ((static_cast<chunk>(1) << bit_shift) - 1);
+        data[c] = (data[c] << bit_shift) | carry;
+        carry = next_carry;
+      }
+    }
+
+    return *this;
+  }
+
+  bitset &operator>>=(size_t shift) {
+    if (!shift || none()) return *this;
+
+    if (shift >= size()) {
+      reset();
+      return *this;
+    }
+
+    size_t chunk_shift = shift / BITS_PER_CHUNK;
+    size_t bit_shift = shift % BITS_PER_CHUNK;
+
+    if (chunk_shift > 0) {
+      for (size_t c = 0; c < n_chunks - chunk_shift; ++c) {
+        data[c] = data[c + chunk_shift];
+      }
+      memset(data + n_chunks - chunk_shift, 0, chunk_shift * sizeof(chunk));
+    }
+
+    if (bit_shift > 0) {
+      chunk carry{};
+      for (size_t c = n_chunks; c-- > 0;) {
+        chunk next_carry = data[c] << (BITS_PER_CHUNK - bit_shift);
+        data[c] = (data[c] >> bit_shift) | (carry << (BITS_PER_CHUNK - bit_shift));
+        carry = next_carry;
+      }
+    }
+
+    return *this;
+  }
+
+  friend bitset operator<<(bitset lhs, size_t shift);
+  friend bitset operator>>(bitset lhs, size_t shift);
+
+  [[nodiscard]] bool test(const size_t &bit) const {
+    assert(bit < n_bits);
+    return data[chunk_index(bit)] &
+            (static_cast<chunk>(1) << bit_index(bit));
+  }
+
+  void flip(const size_t &bit) {
+    assert(bit < n_bits);
+    set(bit, test(bit));
+  }
+
+  void flip_all() {
+    for (size_t c = 0; c < n_chunks; ++c) {
+      data[c] = ~data[c];
     }
   }
 
+  void set(const size_t &bit, bool value = true) {
+    assert(bit < n_bits);
+    if (value) {
+      data[chunk_index(bit)] |= (static_cast<chunk>(1) << bit_index(bit));
+    } else {
+      data[chunk_index(bit)] &= ~(static_cast<chunk>(1) << bit_index(bit));
+    }
+  }
+
+  void set_all(bool value = true) {
+    std::memset(data, value ? 0xFF : 0, n_chunks * sizeof(chunk));
+  }
+
   size_t count() {
-    // Keep counting leading ones
+    size_t res{};
+    for (size_t c = 0; c < n_chunks; ++c) {
+      res += __builtin_popcount(data[c]);
+    }
+    return res;
   }
 
   void reset() {
-    std::memset(data, 0, (B + BITS_PER_CHUNK - 1) / BITS_PER_CHUNK);
+    std::memset(data, 0, n_chunks * sizeof(chunk));
   }
-  // all, any, none
+  
+  bool all() {
+    for (size_t c = 0; c < n_chunks; ++c) {
+      if (data[c] != CHUNK_MAX) return false;
+    }
+    return true;
+  }
+
+  bool any() {
+    for (size_t c = 0; c < n_chunks; ++c) {
+      if (data[c]) return true;
+    }
+    return false;
+  }
+
+  bool none() {
+    for (size_t c = 0; c < n_chunks; ++c) {
+      if (data[c]) return false;
+    }
+    return true;
+  }
+
+  [[nodiscard]] size_t size() const {
+    return n_bits;
+  }
+
+  void print() const {
+    size_t bit = n_bits - 1;
+    for (int c = n_chunks - 1; c >= 0; --c) {
+      for (; bit >= 0 && bit / BITS_PER_CHUNK == c; --bit) {
+        std::cout << static_cast<bool>(data[c] & static_cast<chunk>(1) << (bit % BITS_PER_CHUNK));
+      }
+    }
+  }
+
 };
 
+inline bitset operator<<(bitset lhs, size_t shift) {
+  return lhs <<= shift;
 }
+
+inline bitset operator>>(bitset lhs, size_t shift) {
+  return lhs <<= shift;
+}
+
+} // namespace shiv
--- bitset/main.cpp
+++ bitset/main.cpp
@@ -1,7 +1,60 @@
-#include "bitset.hpp"
+#include <cassert>
 #include <iostream>
+#include "bitset.hpp"
 
 int main() {
-  // Add your code here
-  return 0;
+  shiv::bitset bs(100);
+  bs.set_all();
+  bs.print();
+  std::cout << '\n';
+  bs <<= 64;
+  bs.print();
+  std::cout << '\n';
+  bs >>= 64;
+  bs.print();
+  std::cout << '\n';
+  bs.flip_all();
+  bs.print();
+
+  // set different bits
+  for (size_t i = 0; i < bs.size(); ++i) {
+    bs[i] = static_cast<bool>(i % 2);
+  }
+
+  // check bits
+  for (size_t i = 0; i < bs.size(); ++i) {
+    assert(static_cast<bool>(bs[i]) == (i % 2));
+  }
+
+  // set bits to the same thing to make sure they don't flip
+  for (size_t i = 0; i < bs.size(); ++i) {
+    bs[i] = static_cast<bool>(i % 2);
+  }
+  // check bits
+  for (size_t i = 0; i < bs.size(); ++i) {
+    assert(static_cast<bool>(bs[i]) == (i % 2));
+  }
+
+  // check assignment operator and destructor
+  {
+    shiv::bitset temp(0);
+    temp = bs;
+    // check bits of the temp;
+    for (size_t i = 0; i < bs.size(); ++i) {
+      assert(static_cast<bool>(bs[i]) == (i % 2));
+    }
+  }
+
+  // check int assignment operator
+  bs[3] = 0;
+  assert(bs[3] == 0);
+
+  // check equality operators
+  assert(bs[0] != bs[1]);
+  assert(bs[0] == bs[2]);
+
+  assert(bs[0] != true);
+  assert(bs[1] == true);
+
+  std::cout << "\nPASS\n" << '\n';
 }
