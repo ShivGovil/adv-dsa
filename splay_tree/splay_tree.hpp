5c98467acdd31e4a1f6784e6233f65bedcd25d40
Shiv Govil <shivgov@umich.edu>
2025-05-14 01:39:00 -0400

    could be cool to actually do this at some point

--- splay_tree/splay_tree.hpp
+++ splay_tree/splay_tree.hpp
@@ -1,40 +1,28 @@
 #pragma once
 
-#include <memory>
 template <typename K, typename V>
 class splay_tree {
-  public:
-    splay_tree() : root(nullptr) {}
+public:
+  splay_tree() = default;
 
-    void push(const K &key, const V &value) {
-      if (!root) {
-        root = make_shared<node>(node{key, value, nullptr, nullptr, nullptr});
-        return;
-      }
+  void push(const K& key, const V& value) {}
 
-    }
+  void remove(const K& key) {}
 
-    void remove(const K &key) {
+  V& find(const K& key) {}
 
-    }
+  const V& find(const K& key) const {}
 
-    V& find(const K &key) {
+private:
+  struct node {
+    K key;
+    V value;
+    node *left, *right; // Parent?
+  };
 
-    }
+  // height calc for balancing
+  // rotation functions
+  // (swim up, swim down)
 
-    const V& find(const K &key) {
-
-    }
-
-  private:
-    struct node {
-      K key;
-      V value;
-      std::shared_ptr<node> left, right;
-      // If the parent ptr was shared, then we would have circular references
-      // This would hinder the destruction process
-      std::weak_ptr<node> parent;
-    };
-
-    std::shared_ptr<node> root;
+  node *root;
 };
