b10823ae51adf6dd85d030563a61513e1ff9d1ca
Shiv Govil <shivgov@umich.edu>
2025-05-16 23:10:38 -0400

    function stubs for splay

--- splay_tree/splay_tree.hpp
+++ splay_tree/splay_tree.hpp
@@ -1,28 +1,44 @@
 #pragma once
 
-template <typename K, typename V>
+#include <cstddef>
+#include <functional>
+
+template <typename T, typename Comp = std::less<T>>
 class splay_tree {
-public:
-  splay_tree() = default;
-
-  void push(const K& key, const V& value) {}
-
-  void remove(const K& key) {}
-
-  V& find(const K& key) {}
-
-  const V& find(const K& key) const {}
-
-private:
   struct node {
-    K key;
-    V value;
-    node *left, *right; // Parent?
+    T key;
+    node *left, *right, *parent;
   };
 
-  // height calc for balancing
-  // rotation functions
-  // (swim up, swim down)
-
   node *root;
+  size_t sz{};
+
+  node* min_in_tree(node *root) {
+    while (root) root = root->left;
+    return root;
+  }
+  
+  node* max_in_tree(node *root) {
+    while (root) root = root->right;
+    return root;
+  }
+
+  void rotate_left(node *x);
+  void rotate_right(node *x);
+
+  void replace(node *x, node *y);
+
+  void splay(node *x);
+
+public:
+  void insert(const T &key);
+  node* find(const T &key);
+  void erase(const T &key);
+  node* join(node *first, node *second);
+  std::pair<node*, node*> split(node *root);
+  
+  const T& maximum(const T &key) const { return max_in_tree(key)->val; }
+  const T& minimum(const T &key) const { return min_in_tree(key)->val; }
+  size_t size() { return sz; }
+  bool empty() { return !root; }
 };
