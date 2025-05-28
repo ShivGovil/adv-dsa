8f5f42d7db7d267717e7df6e2d74a6912b57f7aa
Shiv Govil <shivgovil@Shivs-MacBook-Pro.local>
2025-05-28 14:15:36 -0400

    random

--- bloom_filter/bloom_filter.hpp
+++ bloom_filter/bloom_filter.hpp
@@ -1,29 +1,15 @@
-#ifndef bloom_filter_HPP
-#define bloom_filter_HPP
+#pragma once
 
 #include <concepts>
 #include <type_traits>
 
-template <typename T>
-concept primitive = std::is_arithmetic_v<T>;
+namespace shiv {
 
-template <typename T>
-concept composite = requires(T v) {
-  { v.data() } -> std::convertible_to<const void*>;
-  { v.size() } -> std::convertible_to<std::size_t>;
-};
-
-template <typename T>
-concept hashable = primitive<T> || composite<T>;
-
-template <hashable T>
 class bloom_filter {
-  public:
-    bloom_filter();
-    ~bloom_filter();
 
-  private:
-    // Add private members here
+public:
+  bloom_filter();
+  ~bloom_filter();
 };
 
-#endif // bloom_filter_HPP
+}
