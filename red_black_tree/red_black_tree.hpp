19a9eb97b47787426afc2a2dbf5c5031878b8ae4
Shiv Govil <shivgov@umich.edu>
2025-05-29 13:58:23 -0400

    begin

--- red_black_tree/red_black_tree.hpp
+++ red_black_tree/red_black_tree.hpp
@@ -1,13 +1,40 @@
-#ifndef red_black_tree_HPP
-#define red_black_tree_HPP
+#pragma once
 
+#include <cstddef>
+#include <cstdint>
+
+namespace shiv {
+
+enum class Color : uint8_t {
+  BLACK = 0,
+  RED = 1
+};
+
+enum class Direction : uint8_t {
+  LEFT = 0,
+  RIGHT = 1
+};
+
+template<typename T>
 class red_black_tree {
+  struct node {
+    node *parent;
+
+    union {
+      struct {
+        node *left, *right;
+      } lr;
+      node *child[2];
+    } children;
+
+    Color color;
+    T val;
+  };
+
+
   public:
     red_black_tree();
     ~red_black_tree();
-
-  private:
-    // Add private members here
 };
 
-#endif // red_black_tree_HPP
+} // namespace
