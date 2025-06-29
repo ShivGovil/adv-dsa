e9dd70f94b14f06cdd0b3a0dc11c2e4a6868a1a8
Shiv Govil <shivgov@umich.edu>
2025-06-29 13:55:10 -0400

    basic bloom filter test

--- bloom_filter/main.cpp
+++ bloom_filter/main.cpp
@@ -1,8 +1,11 @@
+#include <cassert>
 #include <iostream>
 #include "bloom_filter.hpp"
 
 int main() {
-  shiv::bloom_filter bf;
-  std::cout << "bloom\n";
+  shiv::bloom_filter bf(1000, 0.0001);
+  bf.insert("hello");
+  assert(bf.contains("hello"));
+  assert(!bf.contains("Hello"));
   return 0;
 }
